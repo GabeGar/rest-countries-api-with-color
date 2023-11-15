6811651441dfe4e970b103fe57aa456f8962761e
Gabegar <gagarcia190@gmail.com>
2023-11-15 18:52:32 -0500

    init project setup commit

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+import tailwindcssForms from '@tailwindcss/forms';
+
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['NunitoSans', ...defaultTheme.fontFamily.sans],
+            },
+
+            colors: {
+                colorText: 'hsl(var(--color-text) / <alpha-value>)',
+                colorBg: 'hsl(var(--color-bg) / <alpha-value>)',
+                colorElement: 'hsl(var(--color-element) / <alpha-value>)',
+                colorInput: 'hsl(var(--color-input) / <alpha-value>)',
+            },
+        },
+    },
+    plugins: [
+        tailwindcssForms({
+            strategy: 'base',
+        }),
+    ],
+} satisfies Config;
