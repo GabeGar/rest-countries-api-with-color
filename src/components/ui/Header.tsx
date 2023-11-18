67333dfab131c16dcb9767bd30f28baa2387e5cf
Gabegar <gagarcia190@gmail.com>
2023-11-17 19:54:25 -0500

    updates styling on header to add border shadow

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,7 +2,7 @@ import DarkModeToggle from '../DarkModeToggle';
 
 const Header = () => {
     return (
-        <header className="flex items-center justify-between bg-colorElement px-5 py-6">
+        <header className="flex items-center justify-between bg-colorElement px-5 py-6 shadow-md">
             <h2 className="font-extrabold text-colorText">
                 Where in the world?
             </h2>
