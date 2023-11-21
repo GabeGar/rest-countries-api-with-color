895b812390796b0709461f437c01c7ea9356388e
Gabegar <gagarcia190@gmail.com>
2023-11-21 16:11:32 -0500

    adds react query to manage api state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import AppLayout from './components/ui/AppLayout';
 import Home from './pages/Home';
 
+const queryClient = new QueryClient();
+
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<AppLayout />}>
-                    <Route index element={<Home />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools buttonPosition="bottom-left" />
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<AppLayout />}>
+                        <Route index element={<Home />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     );
 };
 
