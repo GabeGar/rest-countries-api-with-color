009e9a5ef28c068e39b7d9146c24b2d3a8b42c08
Gabegar <gagarcia190@gmail.com>
2023-11-21 18:19:31 -0500

    minor style update on header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,9 +3,9 @@ import DarkModeToggle from '../DarkModeToggle';
 const Header = () => {
     return (
         <header className="flex items-center justify-between bg-colorElement px-5 py-6 shadow-md">
-            <h2 className="font-extrabold text-colorText">
+            <h1 className="font-extrabold text-colorText">
                 Where in the world?
-            </h2>
+            </h1>
 
             <DarkModeToggle />
         </header>
