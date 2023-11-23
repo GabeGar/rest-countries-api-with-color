5a8495c5852fa057664a2698c9d0f9922869fd4f
Gabegar <gagarcia190@gmail.com>
2023-11-22 23:24:10 -0500

    updates tailwind config - incorp hover styles when supported

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import tailwindcssForms from '@tailwindcss/forms';
 
 export default {
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
