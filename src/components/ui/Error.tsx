2ae5e447f4492c950e5c8ba732096c2a7738f033
Gabegar <gagarcia190@gmail.com>
2023-11-25 19:14:51 -0500

    adds backbtn to error component

--- src/components/ui/Error.tsx
+++ src/components/ui/Error.tsx
@@ -1,12 +1,17 @@
+import BackButton from './BackButton';
+
 interface Props {
     message: string;
 }
 
 const Error = ({ message }: Props) => {
     return (
-        <p className="mt-4 flex justify-center text-2xl font-semibold text-colorText">
-            {message}
-        </p>
+        <>
+            <BackButton />
+            <p className="mt-4 flex justify-center text-2xl font-semibold text-colorText">
+                {message}
+            </p>
+        </>
     );
 };
 
